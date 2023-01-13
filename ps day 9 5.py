4909c3f5f2f26f77bd879183658b64ca87c44cbc
anushka2621 <114645211+anushka2621@users.noreply.github.com>
2023-01-13 13:50:03 +0530

    Add files via upload

---
+++ ps day 9 5.py
@@ -0,0 +1,14 @@
+def numberOfSteps (self, num: int) -> int:
+    steps = 0
+    while num > 0:
+        if num % 2 == 0:
+             num /= 2
+        else:
+            num -=1
+                 
+        steps += 1
+        return steps
+
+ans = Solution()
+print(ans.numberOfSteps(14))
+print(ans.numberOfSteps(8))
